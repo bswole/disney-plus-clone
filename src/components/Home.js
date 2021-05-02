5f37723a6ddf5e500a85800f93958e78eef63511
Shawn Kristek <shawnkristek@gmail.com>
2021-05-02 12:16:32 -0600

    more tinkering with overflow, extra movie

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,7 +20,7 @@ const Container = styled.main`
   min-height: calc(100vh - 70px);
   padding: 0 calc(3.5vw + 5px);
   position: relative;
-  overflow: hidden;
+  overflow-x: hidden;
 
   &:before {
     background: url("/images/home-background.png") center center / cover no-repeat fixed;
