79fa4ae7d239fa3f3103cd66115bb8e08cf68d7a
Shawn Kristek <shawnkristek@gmail.com>
2021-05-02 12:12:38 -0600

    Home overflow adjustments

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 import ImgSlider from './ImgSlider';
 import Viewers from './Viewers';
+import Movies from './Movies';
 
 function Home() {
   return (
     <Container>
       <ImgSlider />
       <Viewers />
+      <Movies />
     </Container>
   );
 }
@@ -18,7 +20,7 @@ const Container = styled.main`
   min-height: calc(100vh - 70px);
   padding: 0 calc(3.5vw + 5px);
   position: relative;
-  overflow-x: hidden;
+  overflow: hidden;
 
   &:before {
     background: url("/images/home-background.png") center center / cover no-repeat fixed;
